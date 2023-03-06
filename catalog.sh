3a25639f19c8550f6a7da483402991bcfd2dfe64
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-03-06 21:04:23 +0100

    Update

---
+++ catalog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+set -o pipefail
+
+CATALOGIST_API_KEY="${1}"
+if [[ $CATALOGIST_API_KEY ]]; then echo "CATALOGIST_API_KEY is set"; else echo "CATALOGIST_API_KEY is not set"; fi
+
+CATALOGIST_API_KEY="S_a@rI8OtHL2R3vawir0triVecOx7jak"
+CATALOGIST_ENDPOINT="https://catalogist.lensdx.app/"
+
+if [[ $CATALOGIST_ENDPOINT ]] && [[ $CATALOGIST_API_KEY ]]; then
+  if ls manifest.json 1>/dev/null 2>&1; then
+    echo "Uploading service metadata to Catalogist service..."
+    curl -X POST "${CATALOGIST_ENDPOINT}" -d "@manifest.json" -H "Authorization: ${CATALOGIST_API_KEY}"
+  fi
+fi
---
+++ standards.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+
+set -o pipefail
+
+STANDARDS_API_KEY="${1}"
+if [[ $STANDARDS_API_KEY ]]; then echo "STANDARDS_API_KEY is set"; else echo "STANDARDS_API_KEY is not set"; fi
+
+STANDARDS_API_KEY=""
+STANDARDS_ENDPOINT="https://standards.lensdx.app/"
+
+PRODUCT_NAME="${2}"
+echo "Product name is $PRODUCT_NAME"
+
+if [[ $STANDARDS_ENDPOINT ]] && [[ $STANDARDS_API_KEY ]]; then
+  if [[ -f "standardlint.json" ]]; then
+    npm install standardlint
+    npx standardlint --output
+  fi
+
+  if [[ -f "standardlint.results.json" ]]; then
+    RESULTS=$(jq -r . standardlint.results.json)
+    STANDARDS_PAYLOAD="$(jq -n -c --arg repo $PRODUCT_NAME --argjson st "$RESULTS" '{ repo: $repo, standards: $st }')"
+
+    echo "Uploading standards results to Standards service..."
+    curl -X POST "${STANDARDS_ENDPOINT}" -d "STANDARDS_PAYLOAD" -H "Authorization: ${STANDARDS_API_KEY}" # https://eo5aftcjmcd5p59.m.pipedream.net, @manifest.json
+  fi
+fi
